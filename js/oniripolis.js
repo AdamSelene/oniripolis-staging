d42662d9ffb1ccb6a26d3b6fbc54c59e62b57a29
AdamSelene <olivier.sambourg@gmail.com>
2014-06-29 10:50:52 +0200

    Redesigned home page

--- js/oniripolis.js
+++ js/oniripolis.js
@@ -1,3 +1,15 @@
+$.ajaxChimp.translations = {
+    'fr': {
+        'submit': 'Enregistrement en cours...',
+        0: 'Nous vous avons envoyé un e-mail de confirmation',
+        1: 'S\'il vous plaît entrer une valeur',
+        2: 'Une adresse e-mail doit contenir un seul @',
+        3: 'La partie domaine de l\'adresse e-mail n\'est pas valide (la partie après le @:)',
+        4: 'La partie nom d\'utilisateur de l\'adresse email n\'est pas valide (la partie avant le signe @:)',
+        5: 'Cette adresse e-mail semble faux ou non valides. S\'il vous plaît entrer une adresse email valide'
+    }
+};
+
 $(document).ready(function() {
 	$('.caption-excerpt .btn').click(
 		function(e) {
@@ -15,5 +27,19 @@ $(document).ready(function() {
     $("#signup").on('shown.bs.modal', function() {
         $(this).find("#signup-email").focus();
     });
+	
+	
+	$('#mc-form').ajaxChimp({
+		url: 'http://oniripolis.us3.list-manage.com/subscribe/post?u=6d11ff66b50d743170407d548&amp;id=77ab1d1f01',
+		callback: function(resp) {
+			if (resp.result == 'error') {
+				$('#signup .form-group').addClass('has-error');
+			} else {
+				$('#signup .form-group').removeClass('has-error').addClass('has-success');
+			}
+		},
+		language: 'fr'
+	});
+
 	
 });
