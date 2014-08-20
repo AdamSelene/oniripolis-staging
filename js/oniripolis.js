902c147911a0799549df0d7b39d9d1c0fa5467ae
AdamSelene <adam.selene@oniripolis.fr>
2014-08-20 18:24:17 +0200

    Still more mailchimp integration

--- js/oniripolis.js
+++ js/oniripolis.js
@@ -35,19 +35,26 @@ $(document).ready(function() {
 	$('#mc-form').ajaxChimp({
 		url: 'http://oniripolis.us3.list-manage.com/subscribe/post?u=6d11ff66b50d743170407d548&amp;id=77ab1d1f01',
 		callback: function(resp) {
+			$('#signup label.sr-only').removeClass('sr-only');
 			if (resp.result == 'error') {
-				$('#signup .form-group').addClass('has-error');
+				$('#signup .form-group.required').addClass('has-error');
 			} else {
 				$('#signup .form-group').removeClass('has-error').addClass('has-success');
+				$("#signup").modal("hide");
+				$("#confirm").modal("show");
 			}
 		},
 		language: 'fr'
 	});
 	
-	if (window.location.hash == "#confirm") {
+	if (window.location.hash == "#signup") {
+		$("#signup").modal("show");
+	} if (window.location.hash == "#confirm") {
 		$("#confirm").modal("show");
 	} else if (window.location.hash == "#welcome") {
 		$("#welcome").modal("show");
+	} else if (window.location.hash == "#updated") {
+		$("#updated").modal("show");
 	} else if (window.location.hash == "#goodbye") {
 		$("#goodbye").modal("show");
 	}
